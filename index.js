8a67751b79b279e7839cff42c5d67f08906b1e79
Alduxsan <aldosantarelli@gmail.com>
2022-01-21 23:03:43 -0300

    navbar glitches corrected

--- index.js
+++ index.js
@@ -4,14 +4,16 @@ import "./components/SubContent.js";
 import "./components/artifacts3d.js"
 
 var navbar = document.getElementById("navbar");
-var sticky =navbar.offsetTop;
+var corrector = document.getElementById("corrector");
+var sticky =500;
 
 window.onscroll = function() {setSticky()};
 
 function setSticky() {
   if (window.scrollY > sticky) {
     navbar.classList.add("sticky")
+    corrector.classList.add("styckyActive")
   } else {
     navbar.classList.remove("sticky");
-  }
-}
+    corrector.classList.remove("styckyActive");
+}}
