aec78d07f3a42c761e7112252be5980e67975943
Alduxsan <aldosantarelli@gmail.com>
2022-03-04 12:18:55 -0300

    carousel added for home

--- index.js
+++ index.js
@@ -28,4 +28,19 @@ function setSticky() {
   }
 }
 
+//carousel in home 
+const images = ["media/portada.JPG","media/portada2.JPG", "media/portada3.jpg"]
 
+const carousel = document.querySelector(".carousel");
+const interval = setInterval(function(){
+    startCarousel();
+}, 6000);
+var index = 1;
+
+function startCarousel(){
+    carousel.style.backgroundImage = `url(${images[index++]})`
+    carousel.classList.remove("fade");
+    void carousel.offsetWidth;
+    carousel.classList.add("fade");
+    if (index > images.length - 1) index = 0;
+}
