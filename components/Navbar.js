a5bee0e2c82c4052ea0c74b99135acbc5a497480
Alduxsan <aldosantarelli@gmail.com>
2022-03-05 15:59:40 -0300

    changed difusion for publications

--- components/Navbar.js
+++ components/Navbar.js
@@ -5,9 +5,9 @@ class Navbar extends HTMLElement {
     super();
   }
 
-  checkActive(section, atribute){
-    if (section === atribute){
-      return "active"
+  checkActive(section, atribute) {
+    if (section === atribute) {
+      return "active";
     }
   }
 
@@ -16,19 +16,38 @@ class Navbar extends HTMLElement {
   }
 
   connectedCallback() {
-    let atribute = this.atribute = this.getAttribute("atribute") ?? "valor default"; //en caso de que no venga un atributo (con key atribute) pondrá este valor default
+    let atribute = (this.atribute =
+      this.getAttribute("atribute") ?? "valor default"); //en caso de que no venga un atributo (con key atribute) pondrá este valor default
     this.render(atribute);
   }
 
   render(atribute) {
     this.innerHTML = `
     <div class="topnav" id="navbar">
-      <div class="link_container ${this.checkActive("home",atribute)}"><a href="/index.html">INICIO</a></div>
-      <div class="link_container ${this.checkActive("aboutUs",atribute)}"><a href="/aboutUs.html">SOBRE NOSOTROS</a></div>
-      <div class="link_container ${this.checkActive("investigations",atribute)}"><a href="/investigations.html">INVESTIGACIONES</a></div>
-      <div class="link_container ${this.checkActive("interactive",atribute)}"><a href="/interactive.html">PREHISTORIA INTERACTIVA</a></div>
-      <div class="link_container ${this.checkActive("difusion",atribute)}"><a href="/difusion.html">DIFUSIÓN</a></div>
-      <div class="link_container ${this.checkActive("gallery",atribute)}"><a href="/difusion.html">GALERÍA DE FOTOS</a></div>
+      <div class="link_container ${this.checkActive(
+        "home",
+        atribute
+      )}"><a href="/index.html">INICIO</a></div>
+      <div class="link_container ${this.checkActive(
+        "aboutUs",
+        atribute
+      )}"><a href="/aboutUs.html">SOBRE NOSOTROS</a></div>
+      <div class="link_container ${this.checkActive(
+        "investigations",
+        atribute
+      )}"><a href="/investigations.html">INVESTIGACIONES</a></div>
+      <div class="link_container ${this.checkActive(
+        "interactive",
+        atribute
+      )}"><a href="/interactive.html">PREHISTORIA INTERACTIVA</a></div>
+      <div class="link_container ${this.checkActive(
+        "difusion",
+        atribute
+      )}"><a href="/publications.html">PUBLICACIONES</a></div>
+      <div class="link_container ${this.checkActive(
+        "gallery",
+        atribute
+      )}"><a href="/difusion.html">GALERÍA DE FOTOS</a></div>
     </div>
    
     <style>
