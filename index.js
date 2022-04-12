e0c9fdf08606da6e10dd7a7bcadfb9a469e700a0
Alduxsan <aldosantarelli@gmail.com>
2022-04-11 23:01:51 -0300

    performance changes for optimized loading in aboutUs

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
+import "./components/ProjectNews.js";
+
 import "./components/Navbar.js";
-import "./components/Profiles.js";
-import "./components/artifacts3d.js";
-import "./components/ProjectListItem.js";
-import "./components/FaunaItem.js";
 import "./components/Title.js";
 import "./components/SubNavbarItem.js";
+
+import "./components/Profiles.js";
+import "./components/ActivitiesItem.js";
+
+import "./components/artifacts3d.js";
+import "./components/FaunaItem.js";
+import "./components/ProjectListItem.js";
+
 import "./components/BookItem.js";
 import "./components/PressItem.js";
-import "./components/ActivitiesItem.js";
 import "./components/SciArticle.js";
 import "./components/BookChapter.js";
-import "./components/ProjectNews.js";
+
 import "./components/CollectionLayout.js";
