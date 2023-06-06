7e6d4be12a79d85d537079d995f8fd56d9af0689
Alduxsan <aldosantarelli@gmail.com>
2023-06-06 19:52:53 -0300

    added footer component

---
+++ components/Footer.js
@@ -0,0 +1,43 @@
+import styles from "./css_components/Footer.css" assert { type: "css" };
+
+class Footer extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+    this.shadowRoot.adoptedStyleSheets.push(styles);
+  }
+
+  getAtt(attr) {
+    let attribute = (this.attribute = this.getAttribute(attr));
+    return attribute;
+  }
+
+  connectedCallback() {
+    this.shadowRoot.innerHTML = `
+    <footer>
+    <div class="info1_container"> <p class="info1">
+    Facultad de Humanidades y Ciencias de la Educación, Edificio Central <br/>
+    Avenida Uruguay 1695 <br/>11200 Montevideo - Uruguay <br/>
+    Tel: (+598) 2409 1104 /05 /06</p> </div>
+    <div class="info2_container"> <p class="info2">
+    Laboratorio de Arqueologí­a y Antropologí­a Biológica <br/>
+    Paysandú s/n (entre Tristán Narvaja y D. Fernández Crespo)<br/> 11200 Montevideo - Uruguay <br/>
+Tel: (+598) 2408 3076 </p> </div>
+    </footer>
+    `;
+  }
+
+  disconnectedCallback() {
+    //implementation
+  }
+
+  attributeChangedCallback(name, oldVal, newVal) {
+    //implementation
+  }
+
+  adoptedCallback() {
+    //implementation
+  }
+}
+
+window.customElements.define("footer-data", Footer);
